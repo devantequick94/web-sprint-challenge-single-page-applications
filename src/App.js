49b68c2e50cf26375074d2bdda1b8de6fd10196c
devantequick <devantequick94@gmail.com>
2022-01-28 06:02:51 -0500

    home link and pizza link works

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
 import React from "react";
+import { Route, Switch, Link } from "react-router-dom";
+import OrderForm from "./OrderForm";
+
 
 const App = () => {
   return (
     <>
-      <header>
+     <header className="App-header">
       <h1>Lambda Eats</h1>
-      </header>
-      <p>You can remove this code and create your own header</p>
+       <p>You can remove this code and create your own header</p>
+       <nav>
+         <Link to="/">Home</Link>
+         <Link to="/orderForm/pizza-form">Pizza Form</Link>
+         <Link to="/orderForm/3"></Link>
+         <Link to="/orderForm/4"></Link>
+       </nav>
+     </header>
+
+     <main>
+       <Switch>
+         <Route exact path="/">
+           <h4>
+            Click on above link for link for home
+           </h4>
+         </Route>
+         <Route exact path ="/orderForm/:pizza-form">
+            <OrderForm />
+         </Route>
+
+       </Switch>
+     </main>
+
+     <footer>
+       Copyright 1994
+     </footer>
+
+   
+
+
+
+
+
+
+
+
     </>
   );
 };
---
+++ src/OrderForm.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+
+
+const OrderForm = (props) => {
+
+    const { pizza } = useParams()
+
+    return(
+        <>hello i am an order form { pizza }</>
+    )
+
+}
+
+
+export default OrderForm;
