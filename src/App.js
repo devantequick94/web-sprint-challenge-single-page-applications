aba624729320db315d18c4eb61ac0e9ead0514db
devantequick <devantequick94@gmail.com>
2022-01-28 04:11:37 -0500

    test push

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import React from "react";
 const App = () => {
   return (
     <>
+      <header>
       <h1>Lambda Eats</h1>
+      </header>
       <p>You can remove this code and create your own header</p>
     </>
   );
